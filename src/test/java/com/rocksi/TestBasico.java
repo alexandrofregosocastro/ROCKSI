92f17b75e5537472aba282083620c804c73cf916
bilileo <l2100754@gmail.com>
2026-04-07 14:04:46 -0700

    1st version .yaml

---
+++ src/test/java/com/rocksi/TestBasico.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestBasico {
+
+    @Test
+    void pruebaBasica() {
+        assertEquals(2, 1 + 1);
+    }
+}
